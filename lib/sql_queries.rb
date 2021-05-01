9360f27afa38f595f3f759b69891cb78199515be
kristeneknorn <kristen.knorn@gmail.com>
2021-05-01 19:57:49 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,8 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts
   "SELECT projects.title, SUM(pledges.amount) FROM projects
   JOIN pledges ON projects.id = pledges.project_id
-  GROUP BY projects.title
-  ORDER BY projects.title ASC"
+  GROUP BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges
@@ -18,8 +17,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges
   FROM users
   INNER JOIN pledges
   ON users.id = pledges.user_id
-  GROUP BY users.name
-  ORDER BY users.name;"
+  GROUP BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
@@ -35,7 +33,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   FROM users
   JOIN pledges ON users.id = pledges.user_id
   GROUP BY users.name
-  ORDER BY SUM(pledges.amount);"
+  ORDER BY users.amount;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
